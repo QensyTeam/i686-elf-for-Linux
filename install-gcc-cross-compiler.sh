a752543862e9d25576e373228612bade4d3ef034
QensyTeam <105591194+QensyTeam@users.noreply.github.com>
2025-02-12 12:24:20 +0300

    Update install-gcc-cross-compiler.sh

--- install-gcc-cross-compiler.sh
+++ install-gcc-cross-compiler.sh
@@ -3,62 +3,143 @@
 # Прерывание выполнения скрипта при ошибке
 set -e
 
-# Директории для исходников и установки
-echo "Создание рабочих директорий и установка пакетов необходимых для сборки"
+# Определение количества ядер процессора для параллельной сборки
+NUM_CORES=$(nproc)
 
+# Директории для исходников и установки
 SRC_DIR=$HOME/src
 CROSS_DIR=$HOME/opt/cross
 
-# Пакеты, необходимые для сборки
-sudo apt update
-sudo apt install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo wget mtools xorriso qemu-system grub-pc-bin dosfstools
+# Переменные для версий GCC и binutils
+BINUTILS_VERSION="2.42"
+GCC_VERSION="13.2.0"
 
-# Создание рабочих директорий
-mkdir -p $SRC_DIR
-mkdir -p $CROSS_DIR
+# Функция для установки пакетов
+install_packages() {
+    echo "Установка необходимых пакетов..."
+    sudo apt update
+    sudo apt install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo wget mtools xorriso qemu-system grub-pc-bin dosfstools
+}
 
-# Загрузка исходных кодов binutils и gcc
-echo "Скачивание и разархивирование необходимых архивов таких как gcc 13.2.0 и binutils 2.42"
-cd $SRC_DIR
-wget https://ftp.gnu.org/gnu/binutils/binutils-2.42.tar.gz
-wget https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.gz
-tar -xvf binutils-2.42.tar.gz
-tar -xvf gcc-13.2.0.tar.gz
+# Функция для удаления установленных файлов и директорий
+cleanup() {
+    echo "Удаление исходных кодов, сборочных директорий и установленных файлов..."
+    rm -rf $SRC_DIR
+    rm -rf $CROSS_DIR
+    echo "Очистка завершена."
+}
 
-# Сборка и установка binutils
-echo "Сборка и установка binutils"
-mkdir -p $SRC_DIR/build-binutils
-cd $SRC_DIR/build-binutils
-$SRC_DIR/binutils-2.42/configure --target=i686-elf --prefix=$CROSS_DIR --with-sysroot --disable-nls --disable-werror
-make
-make install
+# Функция для сборки и установки binutils
+install_binutils() {
+    echo "Скачивание и разархивирование binutils $BINUTILS_VERSION..."
+    cd $SRC_DIR
+    wget "https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.gz"
+    tar -xvf "binutils-$BINUTILS_VERSION.tar.gz"
+    
+    echo "Сборка и установка binutils..."
+    mkdir -p $SRC_DIR/build-binutils
+    cd $SRC_DIR/build-binutils
+    $SRC_DIR/binutils-$BINUTILS_VERSION/configure --target=i686-elf --prefix=$CROSS_DIR --with-sysroot --disable-nls --disable-werror
+    make -j$NUM_CORES
+    make install
+}
 
-# Сборка и установка GCC
-echo "Сборка и установка GCC"
-cd $SRC_DIR/gcc-13.2.0
-./contrib/download_prerequisites
+# Функция для сборки и установки GCC
+install_gcc() {
+    echo "Скачивание и разархивирование gcc $GCC_VERSION..."
+    cd $SRC_DIR
+    wget "https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz"
+    tar -xvf "gcc-$GCC_VERSION.tar.gz"
+    
+    echo "Сборка и установка GCC..."
+    cd $SRC_DIR/gcc-$GCC_VERSION
+    ./contrib/download_prerequisites
 
-mkdir -p $SRC_DIR/build-gcc
-cd $SRC_DIR/build-gcc
-$SRC_DIR/gcc-13.2.0/configure --target=i686-elf --prefix=$CROSS_DIR --disable-nls --enable-languages=c,c++ --without-headers
-make all-gcc
-make all-target-libgcc
-make install-gcc
-make install-target-libgcc
+    mkdir -p $SRC_DIR/build-gcc
+    cd $SRC_DIR/build-gcc
+    $SRC_DIR/gcc-$GCC_VERSION/configure --target=i686-elf --prefix=$CROSS_DIR --disable-nls --enable-languages=c,c++ --without-headers
+    make -j$NUM_CORES all-gcc
+    make -j$NUM_CORES all-target-libgcc
+    make install-gcc
+    make install-target-libgcc
+}
 
-# Настройка окружения
-echo "Настройка окружения"
-if ! grep -q "$CROSS_DIR/bin" ~/.bashrc; then
-    echo 'export PREFIX="$HOME/opt/cross"' >> ~/.bashrc
-    source ~/.bashrc
-    echo 'export TARGET=i686-elf' >> ~/.bashrc
-    source ~/.bashrc
-    echo 'export PATH="$PREFIX/bin:$PATH"' >> ~/.bashrc
-    source ~/.bashrc
-fi
+# Функция для настройки окружения
+setup_environment() {
+    echo "Настройка окружения..."
+    if ! grep -q "$CROSS_DIR/bin" ~/.bashrc; then
+        echo 'export PREFIX="$HOME/opt/cross"' >> ~/.bashrc
+        echo 'export TARGET=i686-elf' >> ~/.bashrc
+        echo 'export PATH="$PREFIX/bin:$PATH"' >> ~/.bashrc
+        source ~/.bashrc
+    fi
+}
 
-# Проверка установки
-echo "Проверка установки"
-i686-elf-gcc --version
+# Функция для проверки установки
+check_installation() {
+    echo "Проверка установки..."
+    i686-elf-gcc --version
+    echo "GCC Cross Compiler для i686-elf успешно установлен."
+}
 
-echo "GCC Cross Compiler для i686-elf успешно установлен."
+# Меню выбора действия
+echo "Выберите действие:"
+echo "1) Установить компилятор"
+echo "2) Удалить компилятор"
+echo "3) Удалить архивы и мусор"
+echo "4) Переустановить компилятор"
+echo "5) Выход"
+read -p "Введите номер действия: " action
+
+case $action in
+    1)
+        # Установка компилятора
+        read -p "Введите версию binutils (по умолчанию $BINUTILS_VERSION): " user_binutils_version
+        read -p "Введите версию GCC (по умолчанию $GCC_VERSION): " user_gcc_version
+        
+        # Если версия была введена, используем её
+        BINUTILS_VERSION=${user_binutils_version:-$BINUTILS_VERSION}
+        GCC_VERSION=${user_gcc_version:-$GCC_VERSION}
+
+        # Установка
+        install_packages
+        mkdir -p $SRC_DIR
+        mkdir -p $CROSS_DIR
+        install_binutils
+        install_gcc
+        setup_environment
+        check_installation
+        ;;
+    2)
+        # Удалить компилятор
+        cleanup
+        ;;
+    3)
+        # Удалить архивы и мусор
+        echo "Удаление архивов..."
+        rm -rf $SRC_DIR/*.tar.gz
+        echo "Удаление мусора..."
+        rm -rf $SRC_DIR
+        rm -rf $CROSS_DIR
+        ;;
+    4)
+        # Переустановка компилятора
+        cleanup
+        install_packages
+        mkdir -p $SRC_DIR
+        mkdir -p $CROSS_DIR
+        install_binutils
+        install_gcc
+        setup_environment
+        check_installation
+        ;;
+    5)
+        # Выход
+        echo "Выход из скрипта."
+        exit 0
+        ;;
+    *)
+        echo "Некорректный выбор. Выход."
+        exit 1
+        ;;
+esac
